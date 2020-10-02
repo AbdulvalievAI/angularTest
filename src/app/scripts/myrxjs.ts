bdc020ae278f7234acdd295dcfea672c2f5145cc
Andrey Abdulvaliev <abdulvalievai@altarix.ru>
2020-10-02 19:10:27 +0400

    скрипт / rxjs - добавил setTimeout

--- src/app/scripts/myrxjs.ts
+++ src/app/scripts/myrxjs.ts
@@ -8,12 +8,22 @@ const myObservable  = new Observable(subscriber => {
   subscriber.next('myObservable 2');
   subscriber.next('myObservable 3');
 });
-myObservable.subscribe(x => console.log(x));
+myObservable.subscribe(v => console.log(`subscribe #1 - ${v}`));
+myObservable.subscribe(v => console.log(`subscribe #2 - ${v}`));
+setTimeout(() => {
+  myObservable.subscribe(v => console.log(`subscribe #3 - ${v}`));
+}, 0);
 // RESULT
 // =============Observable
-// myObservable 1
-// myObservable 2
-// myObservable 3
+// subscribe #1 - myObservable 1
+// subscribe #1 - myObservable 2
+// subscribe #1 - myObservable 3
+// subscribe #2 - myObservable 1
+// subscribe #2 - myObservable 2
+// subscribe #2 - myObservable 3
+// subscribe #3 - myObservable 1
+// subscribe #3 - myObservable 2
+// subscribe #3 - myObservable 3
 
 
 // Subject
@@ -21,15 +31,17 @@ console.log('=============Subject');
 const mySubject = new Subject();
 mySubject.subscribe(v => console.log(`subscribe #1 - ${v}`));
 mySubject.next('mySubject 1');
-mySubject.subscribe(v => console.log(`subscribe #2 - ${v}`));
 mySubject.next('mySubject 2');
+mySubject.subscribe(v => console.log(`subscribe #2 - ${v}`));
+setTimeout(() => {
+  mySubject.subscribe(v => console.log(`subscribe #3 - ${v}`));
+}, 1000);
 mySubject.next('mySubject 3');
-mySubject.subscribe(v => console.log(`subscribe #3 - ${v}`));
+mySubject.subscribe(v => console.log(`subscribe #4 - ${v}`));
 // RESULT
 // =============Subject
 // subscribe #1 - mySubject 1
 // subscribe #1 - mySubject 2
-// subscribe #2 - mySubject 2
 // subscribe #1 - mySubject 3
 // subscribe #2 - mySubject 3
 
@@ -41,8 +53,11 @@ myBehaviorSubject.subscribe(v => console.log(`subscribe #1 - ${v}`));
 myBehaviorSubject.next('myBehaviorSubject 1');
 myBehaviorSubject.next('myBehaviorSubject 2');
 myBehaviorSubject.subscribe(v => console.log(`subscribe #2 - ${v}`));
+setTimeout(() => {
+  myBehaviorSubject.subscribe(v => console.log(`subscribe #3 - ${v}`));
+}, 1000);
 myBehaviorSubject.next('myBehaviorSubject 3');
-myBehaviorSubject.subscribe(v => console.log(`subscribe #3 - ${v}`));
+myBehaviorSubject.subscribe(v => console.log(`subscribe #4 - ${v}`));
 // RESULT
 // =============BehaviorSubject
 // subscribe #1 - myBehaviorSubject 0
@@ -51,8 +66,8 @@ myBehaviorSubject.subscribe(v => console.log(`subscribe #3 - ${v}`));
 // subscribe #2 - myBehaviorSubject 2
 // subscribe #1 - myBehaviorSubject 3
 // subscribe #2 - myBehaviorSubject 3
+// subscribe #4 - myBehaviorSubject 3
 // subscribe #3 - myBehaviorSubject 3
-
 
 
 // ReplaySubject
@@ -60,18 +75,24 @@ console.log('=============myReplaySubject');
 const myReplaySubject = new ReplaySubject();
 myReplaySubject.subscribe(v => console.log(`subscribe #1 - ${v}`));
 myReplaySubject.next('myReplaySubject 1');
-myReplaySubject.subscribe(v => console.log(`subscribe #2 - ${v}`));
 myReplaySubject.next('myReplaySubject 2');
+myReplaySubject.subscribe(v => console.log(`subscribe #2 - ${v}`));
+setTimeout(() => {
+  myBehaviorSubject.subscribe(v => console.log(`subscribe #3 - ${v}`));
+}, 1000);
 myReplaySubject.next('myReplaySubject 3');
-myReplaySubject.subscribe(v => console.log(`subscribe #3 - ${v}`));
+myReplaySubject.subscribe(v => console.log(`subscribe #4 - ${v}`));
 // RESULT
 // =============myReplaySubject
 // subscribe #1 - myReplaySubject 1
-// subscribe #2 - myReplaySubject 1
 // subscribe #1 - myReplaySubject 2
+// subscribe #2 - myReplaySubject 1
 // subscribe #2 - myReplaySubject 2
 // subscribe #1 - myReplaySubject 3
 // subscribe #2 - myReplaySubject 3
+// subscribe #4 - myReplaySubject 1
+// subscribe #4 - myReplaySubject 2
+// subscribe #4 - myReplaySubject 3
 // subscribe #3 - myReplaySubject 1
 // subscribe #3 - myReplaySubject 2
 // subscribe #3 - myReplaySubject 3
